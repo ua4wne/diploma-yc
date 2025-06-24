72e19a3f5f26d54f7810d31294b1af28659465a4
Alexander Rogatnev <rai68@yandex.ru>
2025-06-24 18:28:11 +0000

    edit backend s3

--- terraform/src/k8s/providers.tf
+++ terraform/src/k8s/providers.tf
@@ -7,16 +7,16 @@ terraform {
   required_version = ">=1.6.6"
 
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
-    bucket                      = "s3-tf-backend"
-    region                      = "ru-central1-a"
-    key                         = "diploma/terraform.tfstate"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
-    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+    # endpoints = {
+    #   s3 = "https://storage.yandexcloud.net"
+    # }
+    # bucket                      = "s3-tf-backend"
+    # region                      = "ru-central1-a"
+    # key                         = "diploma/terraform.tfstate"
+    # skip_region_validation      = true
+    # skip_credentials_validation = true
+    # skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    # skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
 
   }
 }
